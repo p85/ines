9f8de7c8e3b9afe21535dc9f464c07c6c660125c
Patrick Przystolik <mtcllf@gmail.com>
2019-08-02 22:26:18 +0200

    find preview

--- src/ines/core.cljs
+++ src/ines/core.cljs
@@ -3,27 +3,25 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (reagent/atom {:searchText ""
+(defonce app-state (reagent/atom {:searchText nil
                                   :showPreview false
-                                  :waren [{:name "Milch"}
-                                          {:name "Brot"}]}))
+                                  :waren [{:name "Milch" :prop 123}
+                                          {:name "Milchiges" :prop 1}
+                                          {:name "Brot" :prop 2}]}))
 
 (defn text-input [text]
   (swap! app-state assoc :searchText text))
 
 (defn preview-component []
-  (when-let [sText (get @app-state :searchText)]
+  (let [sText (get @app-state :searchText)]
     (when-not (empty? sText)
-      (println "X:")
-      (println (re-find (re-pattern (str ".*lch*.*")) sText)))))
-
-    ; (println (some #(= sText (get % :name)) (get @app-state :waren)))))
+      (println (filter #(not (= nil (re-find (re-pattern (str "(?i)" (get % :name))) sText))) (get @app-state :waren))))))
 
 (defn main-component []
   [:div
    [:h1 "ines"]
    [:h3 "bitte etwas eingeben"]
-   [:input {:type "text"  :on-change #(text-input (-> % .-target .-value))}]
+   [:input {:type "text" :on-change #(text-input (-> % .-target .-value))}]
    [:div (preview-component)]])
 
 
