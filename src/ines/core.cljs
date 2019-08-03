d3ea8cc66f90322997bff7517e85348aa733811e
Patrick Przystolik <mtcllf@gmail.com>
2019-08-03 17:45:07 +0200

    adding and deleting, bugfixes

--- src/ines/core.cljs
+++ src/ines/core.cljs
@@ -11,19 +11,35 @@
 (defn text-input [text]
   (swap! app-state assoc :searchText text))
 
-(defn on-click-preview-list [item-name]
+;; add a item
+(defn add-item-from-preview-list [item-name]
   (when-let [currentList (get @app-state :liste)]
     (when-not (some #(= % {:name item-name}) currentList)
       (swap! app-state assoc :liste (conj currentList {:name item-name}))
       (println (get @app-state :liste)))))
 
+;; delete a item
+(defn delete-item [item-name]
+  (when-let [currentList (get @app-state :liste)]
+    (when (some #(= % {:name item-name}) currentList)
+      (swap! app-state assoc :liste (remove #(= {:name item-name} %) currentList)))))
+
 (defn preview-component []
-    (when-let [sText (get @app-state :searchText)]
+  (when-let [sText (get @app-state :searchText)]
+    (when-not (empty? sText)
       (when-let [preview-results (filter #(not (= nil (re-find (re-pattern (str "(?i)" sText))  (get % :name)))) (get @app-state :waren))]
         [:ul
          (for [item preview-results]
-           [:li {:key (:name item) :class "preview-item" :on-click #(on-click-preview-list (:name item))} (:name item)]
-        )])))
+           [:li {:key (:name item) :class "preview-item" :on-click #(add-item-from-preview-list (:name item))} (:name item)]
+           )]))))
+
+(defn list-component []
+  (when-let [currentList (get @app-state :liste)]
+    (when-not (empty? currentList)
+      [:div "Ihr Einkaufszettel:"
+       [:ul
+        (for [item currentList]
+          [:li {:key (:name item) :class "item" :on-click #(delete-item (:name item))} (:name item)])]])))
 
 ;; {:style {:background "red"}}
 (defn main-component []
@@ -31,7 +47,8 @@
    [:h1 "ines"]
    [:h3 "bitte etwas eingeben"]
    [:input {:type "text" :on-change #(text-input (-> % .-target .-value))}]
-   [:div {:class "preview"} (preview-component)]])
+   [:div {:class "preview"} (preview-component)]
+   [:div {:class "list"} (list-component)]])
 
 
 
