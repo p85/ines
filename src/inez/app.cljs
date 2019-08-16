2e418c04dfb83f7c55f7bfb093fc6560989b7965
Patrick Przystolik <mtcllf@gmail.com>
2019-08-16 21:13:00 +0200

    improves #16

--- src/inez/app.cljs
+++ src/inez/app.cljs
@@ -131,8 +131,8 @@
     [:div {:class "input-group-append"}
      (let [show-preview-button-state (get @app-state :showPreview)]
        [:button {:class "btn btn-outline-secondary btn-show-all" :type "button" :id "btn-show-all" :on-click #(show-preview (not show-preview-button-state))} (if (= true show-preview-button-state) "Liste ausblenden" "alle Artikel anzeigen")])]
-    [:div {:class "info-tooltip-element" :data-toggle "tooltip" :title "Gesuchten Artikel eingeben, zB.:\n\nSchokolade\n\noder mit Mengenangabe:\n\n5 Milch"}
-     [:img {:src "img/info.svg" :id "info" :class "info-tooltip-icon"}]]]
+    [:div {:class "info-tooltip-element" :data-toggle "tooltip" :title "Gesuchten Artikel eingeben, zB.:\nSchokolade\noder mit Mengenangabe:\n5 Milch"}
+     [:img {:src "img/info.svg" :class "info-tooltip-icon"}]]]
    [:div {:class "preview"} (preview-component)]
    [:div {:class "list"} (list-component)]])
 
