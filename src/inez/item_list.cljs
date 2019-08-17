9fd95ab3594af4aa33be7419e2c11391a9f32379
Patrick Przystolik <mtcllf@gmail.com>
2019-08-17 09:33:12 +0200

    implements #8

--- src/inez/item_list.cljs
+++ src/inez/item_list.cljs
@@ -1,8 +1,16 @@
 (ns inez.item-list)
 
+;; basically each line represents a item category
 (def item-list [{:name ["Milch"] :units "Liter"}
-                {:name ["Jogurt" "Danone"] :units "Kg"}
-                {:name ["Brot" "Bread" "Vollkorn"] :units "Stück"}
-                {:name ["Reis" "Rice" "Uncle" "Bens"] :units "Beutel"}
-                {:name ["very long shit thing" "Schokolade" "Milka" "Alpia"] :units "Stück"}
+                {:name ["Wasser" "Wasser naturell" "Cola" "Cola Light" "Cola Zero" "Cherry Cola" "Vanilla Cola" "Fanta" "Sprite" "Tee"] :units "Liter"}
+                {:name ["Kaffee" "Marken Kaffee" "Kaffee Pads"] :units "Stück"}
+                {:name ["Jogurt" "Quark" "Kleiner Jogurt" "Milchreis"] :units "Stück"}
+                {:name ["Brot" "Eiweissbrot" "Vollkornbrot" "Roggenbrot" "Weissbrot" "Toastbrot" "Brötchen" "Käsebrotchen" "Croissant"] :units "Stück"}
+                {:name ["Reis" "Vollkorn Reis" "Nudeln" "Vollkorn Nudeln"] :units "Beutel"}
+                {:name ["Schokolade" "Pfefferminz Schokolade" "Erdbeer Schokolade" "Karamell Schokolade" "Edle Pralinen" "Schoko Linsen"] :units "Stück"}
+                {:name ["Kartoffeln" "Süsskartoffeln" "Pommes frites"] :units "Sack"}
+                {:name ["Weingummi" "Gummibärchen" "Lakritz" "Fruchtgummi" "Marshmallows" "Gelee Bananen"] :units "Tüte"}
+                {:name ["Salami" "Mortadella" "Leberwurst" "Schinkenwurst" "Mettwurst"] :units "Packung"}
+                {:name ["Erdbeer Konfitüre" "Nuss Nougat Creme" "Erdnuss Butter"] :units "Glas"}
+                {:name ["Käse" "Edamer Käse" "Maasdamer Käse" "Frischkäse Aufstrich"] :units "Packung"}
                 ])
