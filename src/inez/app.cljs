1d57fef4e59ffe023bdd099057b70f6ca3e10b24
Patrick Przystolik <mtcllf@gmail.com>
2019-08-18 09:43:28 +0200

    removed old code

--- src/inez/app.cljs
+++ src/inez/app.cljs
@@ -1,10 +1,5 @@
 (ns inez.app
   (:require [reagent.core :as reagent])
-  (:require [inez.app-state :as app-state])
-  (:require [inez.pagination-component :as pagination-component])
-  (:require [inez.preview-not-found-component :as preview-not-found-component])
-  (:require [inez.preview-component :as preview-component])
-  (:require [inez.list-component :as list-component])
   (:require [inez.main-component :as main-component]))
 
 (defn start []
